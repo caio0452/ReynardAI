1185aee391aaedcba60583f34653bbeb98e153a4
Caio P. <caio@example.com>
2026-06-27 00:34:56 -0300

    Add __init__.py to every subdir

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="reynard_ai",
     version="0.0.1",
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    packages=find_namespace_packages(where="src"),
 )
